90022c64667ca88ebce69318abcf0ba7fd5887fa
AplyQ8 <kirill.smith.98@inbox.ru>
2021-11-27 22:35:31 +0300

    this shit needs to work

---
+++ SUBD_Lab_1.0/DandT_library/BuisnessLayer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+
+namespace DandT_library
+{
+    public class BuisnessLayer
+    {
+        DataBase db = new DataBase();
+        MySqlDataAdapter adapter = new MySqlDataAdapter();
+        public string passFunction(string login, string password)
+        {
+            string str1 = "пользователь авторизован!\n";
+            string str2 = "неверный логин или пароль\n";
+            
+            DataTable table = new DataTable();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login`= @l AND `password`= @p", db.getConnection());
+            command.Parameters.Add("@l", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@p", MySqlDbType.VarChar).Value = password;
+
+            db.openConnection();
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                db.closeConnection();
+                return str1;
+            }
+            else
+            {
+                db.closeConnection();
+                return str2;
+            }
+
+            
+        }
+        public void UserRegistration(string login, string password)
+        {
+            if (isUserExists(login))
+                return;
+            MySqlCommand command = new MySqlCommand("INSERT INTO `users` (login, password) values (@l, @p)", db.getConnection());
+            command.Parameters.Add("@l", MySqlDbType.VarChar).Value = login;
+            command.Parameters.Add("@p", MySqlDbType.VarChar).Value = password;
+
+            db.openConnection();
+
+            if(command.ExecuteNonQuery() == 1)
+                Console.WriteLine("Пользователь зарегистрирован!\n");
+            else
+                Console.WriteLine("что-то пошло не так...\n");
+
+            db.closeConnection();
+
+            
+
+        }
+        public Boolean isUserExists(string login)
+        {
+            DataTable table = new DataTable();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `users` WHERE `login`= @l", db.getConnection());
+            command.Parameters.Add("@l", MySqlDbType.VarChar).Value = login;
+
+            db.openConnection();
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+            {
+                db.closeConnection();
+                return true;
+            }
+            else
+            {
+                db.closeConnection();
+                return false;
+            }
+        }
+    }
+}
---
+++ SUBD_Lab_1.0/UI_test/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DandT_library;
+
+namespace UI_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BuisnessLayer bl = new BuisnessLayer();
+            string login = Console.ReadLine();
+            string password = Console.ReadLine();
+            string str = bl.passFunction(login, password);
+            Console.WriteLine(str);
+            Console.ReadKey();
+            //bl.UserRegistration("Bob", "Marley");
+            //Console.ReadKey();
+            
+        }
+    }
+}
