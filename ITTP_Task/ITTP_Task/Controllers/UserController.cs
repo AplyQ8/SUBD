c3f409b11ab27de4e052889e8bc97f76ea29acd6
AplyQ8 <kirill.smith.98@inbox.ru>
2022-04-23 19:28:58 +0300

    task

---
+++ ITTP_Task/ITTP_Task/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using ITTP_Task.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using System.Data.SqlClient;
+
+namespace ITTP_Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+
+        public UserController(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(User user, string login, string password)
+        {
+
+        }
+
+        public bool isAdmin(string login, string password)
+        {
+            string query = @"select login, password where login=@l, password=@p, admin=1";
+
+            DataTable dt = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ITTPAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+
+            }
+
+        }
+
+    }
+}
---
+++ ITTP_Task/ITTP_Task/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ITTP_Task.Models
+{
+    public class User
+    {
+        Guid Guid;
+        public string login;
+        public string password;
+        public string name;
+        public int gender;
+        public DateTime birthday;
+        public bool admin;
+        public DateTime createdOn;
+        public string createdBy;
+        public DateTime modifiedOn;
+        public string modifiedBy;
+        public DateTime revokedOn;
+        public string revokedBy;
+    }
+}
